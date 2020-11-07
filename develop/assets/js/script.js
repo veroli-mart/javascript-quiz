3945ebc79339a6845319ef4bbd9d76e560ddbee0
Veronica Martinez <vmartinez6615@gmail.com>
2020-11-07 18:47:32 -0500

    slow beginnings

--- develop/assets/js/script.js
+++ develop/assets/js/script.js
@@ -1,55 +1,62 @@
-var questionOne = [
-    { title: "What is the keyword used when declaring variables that makes them private to a function?",
-    choices: [
-        'var', 
-        'function', 
-        'if'],
-        answer: "var"}
-    ];
-
-var questionTwo = [ 
-    { title:"How do you create a DIV element using JavaScript?",
-    choices: [
+var questions = [
+    { 
+        questionTitle: "What is the keyword used when declaring variables that makes them private to a function?",
+        answer: "var",
+        choices:[
+            'var', 
+            'function', 
+            'if'
+        ],
+    },
+    { 
+        questionTitle:"How do you create a DIV element using JavaScript?",
+        answer: "document.createElement('div')",
+        choices:[
         "document.querySelector('div')",
         "function('div)",
         "document.createElement('div')",
-        "var div = function()", ],
-        answer: "document.createElement('div')"}
-    ]; 
+        "var div = function()", 
+        ],
+    },
 
-var questionThree = [
-    {title: "Inside which HTML element do we put the JavaScript?",
-    choices: [
+    {
+        questionTitle: "Inside which HTML element do we put the JavaScript?",
+        answer: "<script>",
+        choices:[
         "<javascript>",
          "<script>",
          "<js>",
-         "<a href = '' "],
-         answer: "<script"}
-];
-
-var questionFour = [
-    {title: "The size of the letter (upper or lowercase) have no difference in meaning in JavaScript.",
-    choices: [
+         "<a href = '' ",
+        ],
+    },
+    {
+        questionTitle: "The size of the letter (upper or lowercase) have no difference in meaning in JavaScript.",
+        answer: "false",
+        choices:[
         "true",
         "false"
-    ]
-    }
+    ],
+    },
 ];
-
-var timeLeft = 75;
-var timer = setInterval(countDown, 1000);
-
-function myStopFunction() {
-    clearInterval(timer);
-}
-   function countDown(){
-        console.log(timeLeft); 
-    timeLeft--
-    if(timeLeft === 0){
-        myStopFunction();
-        console.log("You ran out of time!")
-    }
+var startQuiz = function() {
+    
 }
 
 
-  
+// var timeLeft = 75;
+// var timer = setInterval(countDown, 1000);
+
+// function myStopFunction() {
+//     clearInterval(timer);
+// }
+//    function countDown(){
+//         console.log(timeLeft); 
+//     timeLeft--
+//     if(timeLeft === 0){
+//         myStopFunction();
+//         console.log("You ran out of time!")
+//     }
+// }
+
+var quizTimer = getElementbyId("timer");
+quizTimer.addEventListener("start", startQuiz);
